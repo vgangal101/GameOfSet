3524b016018c4afc77dbe76e4b4bb9b776f85c3e
vgangal101 <vardaan.gangal@gmail.com>
2020-12-24 16:29:53 -0500

    laying out skeleton

--- src/gameofset_core.py
+++ src/gameofset_core.py
@@ -1,2 +1,25 @@
 import os
 import sys
+
+class Card():
+    def __init__(self,number,shape,shading,color):
+        self.number = number
+        self.shape = shape
+        self.shading = shading
+        self.color = color
+
+    def print_card():
+        print("%s,%s,%s,%s",self.number,self.shape,self.shading,self.color)
+
+
+
+def generate_deck():
+    # TODO
+    return deckOfCards
+
+
+def isSet():
+    # TODO:
+    return
+
+# may need to define aditional methods for isSet
